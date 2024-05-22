e29784808eae2cef4903b5a5626ed309a56f185d
SujaanArora09 <arorasujaan@gmail.com>
2024-05-22 19:29:21 +0530

    inisial commit

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -26,7 +26,7 @@ const OrderReceivedEmail = ({
   const baseUrl =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'http://localhost:3000'
+      : 'https://gotucovered.vercel.app/'
 
   return (
     <Html>
