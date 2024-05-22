c50ed0455a2888405d52c84d8c31342f11962ba3
SujaanArora09 <arorasujaan@gmail.com>
2024-05-22 18:52:19 +0530

    initial commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,41 @@
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export const formatPrice = (price: number) => {
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'INR',
+  })
+
+  return formatter.format(price)
+}
+
+export function constructMetadata({
+  title = 'Got`U`Covered',
+  description = 'custom phone cases',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    
+    icons,
+    metadataBase: new URL("http://localhost:3000")
+  }
+}
